9eca2dacf024d0990c2ad0f9b5c796f7cb15b29c
rhquddnr95 <39820865+rhquddnr95@users.noreply.github.com>
2023-05-11 23:32:20 +0900

    Refactor: 마이페이지 네비게이션 리팩토링

--- FrontEnd/src/components/user/my-page-component/my-page-nav.jsx
+++ FrontEnd/src/components/user/my-page-component/my-page-nav.jsx
@@ -1,67 +1,79 @@
 import { useRef, useState } from "react";
 import { Link } from "react-router-dom";
+
+/** 마이페이지 네비게이션 컴포넌트*/
 const MyPageNav = () => {
-  const linkRef1 = useRef();
-  const linkRef2 = useRef();
-  const linkRef3 = useRef();
-  const linkRef4 = useRef();
-  const linkRef5 = useRef();
+
+  const orderedItemsListRef = useRef();
+  const personalInfoRef = useRef();
+  const personalInfoModifyRef = useRef();
+  const cusServiceCenterRef = useRef();
+  const withdrawlRef = useRef();
   const [color] = useState("text-c1");
+
+  /** 네비게이션 핸들러 onClick시 색상변경*/
   const navBarHandler = (e) => {
-    linkRef1.current.style.color = "#c9c9c9";
-    linkRef2.current.style.color = "#c9c9c9";
-    linkRef3.current.style.color = "#c9c9c9";
-    linkRef4.current.style.color = "#c9c9c9";
-    if (e.target != linkRef5.current) {
+    orderedItemsListRef.current.style.color = "#c9c9c9";
+    personalInfoRef.current.style.color = "#c9c9c9";
+    personalInfoModifyRef.current.style.color = "#c9c9c9";
+    cusServiceCenterRef.current.style.color = "#c9c9c9";
+    if (e.target != withdrawlRef.current) {
       //회원탈퇴 요소가 아니면 검은색으로
       e.target.style.color = "#060606";
     }
   };
-  const current = window.location.href;
+
+    // url에 따른 네비 색상 변경 (아직 미구현)
+    // const current = window.location.href;
 
   return (
     <>
       <div className="w-[180px] flex flex-col m-[10px] mb-[30px]  border-r-[1px] border-barColor justify-between">
         <ul className="relative left-0 flex flex-col ">
           <li className=" mb-[20px]">
+            {/* 장바구니 네비 링크 */}
             <Link to="/cart" className={color}>
               장바구니
             </Link>
           </li>
+          {/* 주문내역 네비 링크 */}
           <li className=" mb-[20px]">
             <Link
               to="/mypage/ordered-items-list"
-              ref={linkRef1}
+              ref={orderedItemsListRef}
               onClick={navBarHandler}
               className="text-c2"
             >
               주문내역
             </Link>
           </li>
+          {/* 내 정보 네비 링크*/}
           <li className=" mb-[20px]">
             <Link
               to="/mypage/personal-info"
-              ref={linkRef2}
+              ref={personalInfoRef}
               onClick={navBarHandler}
               className={color}
             >
               내 정보
             </Link>
           </li>
+          {/* 개인정보 수정 네비 링크*/}
           <li className=" mb-[20px]">
             <Link
               to="/mypage/personal-info-modify"
-              ref={linkRef3}
+              ref={personalInfoModifyRef}
               onClick={navBarHandler}
               className={color}
             >
               개인정보 수정
             </Link>
           </li>
+          {/* 고객센터 네비 링크*/}
           <li className=" mb-[20px] ">
             <Link
               to="/mypage/cus-service-center"
-              ref={linkRef4}
+              ref={cusServiceCenterRef}
               onClick={navBarHandler}
               className={color}
             >
@@ -69,10 +81,11 @@ const MyPageNav = () => {
             </Link>
           </li>
         </ul>
+        {/* 회원탈퇴 네비 링크*/}
         <div>
           <Link
             to="/mypage/withdrawl"
-            ref={linkRef5}
+            ref={withdrawlRef}
             onClick={navBarHandler}
             className="text-c3"
           >
