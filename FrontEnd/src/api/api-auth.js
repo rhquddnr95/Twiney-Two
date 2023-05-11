18bbe72d9b8a855b394664c006e75b20220b58d0
jang_yoonsu <shagrat8546@gmail.com>
2023-05-11 23:28:29 +0900

    Refactor : api-auth.js 파일 리팩토링

--- FrontEnd/src/api/api-auth.js
+++ FrontEnd/src/api/api-auth.js
@@ -1,16 +1,16 @@
 import axios from "axios";
 
+const axiosBase = axios.create({
+  baseURL: `http://${window.location.hostname}:5000/`,
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${JSON.parse(localStorage.getItem("token"))}`,
+  },
+});
+
 export const getUserDataByToken = async () => {
   if (JSON.parse(localStorage.getItem("token"))) {
-    const data = await axios.get(
-      "http://34.22.85.44:5000/api/users/auth/verifyToken",
-      {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${JSON.parse(localStorage.getItem("token"))}`,
-        },
-      }
-    );
+    const data = await axiosBase.get(`api/users/auth/verifyToken`);
     return data.data;
   } else {
     return;
@@ -19,12 +19,7 @@ export const getUserDataByToken = async () => {
 
 export const deleteUserDataByToken = async () => {
   if (JSON.parse(localStorage.getItem("token"))) {
-    const data = await axios.delete("http://34.22.85.44:5000/api/users/", {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${JSON.parse(localStorage.getItem("token"))}`,
-      },
-    });
+    const data = await axiosBase.delete("http://34.22.85.44:5000/api/users/");
     return data.data;
   } else {
     return;
