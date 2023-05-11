8414372038ccb6fcff9f9bada192cf2d913f41af
jang_yoonsu <shagrat8546@gmail.com>
2023-05-12 00:11:54 +0900

    Refactor : api폴더 api파일들  리팩토링

--- FrontEnd/src/api/api-category.js
+++ FrontEnd/src/api/api-category.js
@@ -1,6 +1,7 @@
-import axios from "axios";
+import { axiosBase } from "./api";
 
+/** 모든 카테고리 데이터 요청 */
 export const getAllCategories = async () => {
-  const data = await axios.get(`http://34.22.85.44:5000/api/categories`);
+  const data = await axiosBase.get(`api/categories`);
   return data.data;
 };
