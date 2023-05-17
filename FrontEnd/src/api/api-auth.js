f9c3cc640ff4ef2f0afbc3210dc012472c0159a4
이수현 <lsh@isuhyeon-ui-MacBookAir.local>
2023-05-18 04:26:29 +0900

    Refactor : api-auth, auth-context 컴포넌트명 변경 및 주석

--- FrontEnd/src/api/api-auth.js
+++ FrontEnd/src/api/api-auth.js
@@ -1,6 +1,6 @@
 import { axiosBaseWithToken } from "./api";
 
-/** 로컬스토리지에 저장된 토큰에 해당하는 유저데이터 요청*/
+/** 로컬스토리지에 저장된 토큰에 해당하는 유저데이터 요청 */
 export const getUserDataByToken = async () => {
   if (JSON.parse(localStorage.getItem("token"))) {
     const data = await axiosBaseWithToken.get(`api/users/auth/verifyToken`);
@@ -10,7 +10,7 @@ export const getUserDataByToken = async () => {
   }
 };
 
-/** 로컬스토리지에 저장된 토큰에 해당하는 유저데이터 삭제 요청*/
+/** 로컬스토리지에 저장된 토큰에 해당하는 유저데이터 삭제 요청 */
 export const deleteUserDataByToken = async () => {
   if (JSON.parse(localStorage.getItem("token"))) {
     const data = await axiosBaseWithToken.delete("api/users/");
